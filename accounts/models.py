52e92cadeed3dc18f8be3f28bd3a00585ca18193
VISHAL SINGH <56965195+vishalsingha@users.noreply.github.com>
2021-01-04 12:16:00 +0530

    Add files via upload

---
+++ accounts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class massage_request(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.ImageField(max_length=10)
+    message = models.CharField(max_length=500)
+    
+
+
---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register', views.register, name = 'accounts-register'),
+    path('login', views.login, name = 'accounts-login'),
+    path('logout', views.logout, name = 'accounts-logout'),
+    path('massage', views.massage, name = 'accounts-massage'),
+    path('checkotp', views.check_otp, name = 'accounts-checkotp'),
+
+]
+
